71f570e2d7f5179376047a8280e1963612d137bc
Gasan <1793959@mail.rum>
2023-02-18 21:21:27 +0300

    new commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, 
+// является ли этот день выходным.
+
+string[]week = new string [7] {"Понедельник", "Вторник", "Стреда", "Четверг", "Пятница", "Суббота", "Всокресенье" };
+while (true)
+{
+  Console.WriteLine("Введите число от 1 до 7");
+  int userNumber = Convert.ToInt32(Console.ReadLine());
+  if (userNumber < 7 || userNumber > 7 )
+  {
+    Console.WriteLine("этот день не выходной");
+    userNumber = Convert.ToInt32(Console.ReadLine ());
+  }  
+else
+{
+    int weekIndex = userNumber -1;
+    Console.WriteLine($"Этот день {userNumber} выходной");
+    break;
+}
+
+}
